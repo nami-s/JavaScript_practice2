db6594dbcb7071f55ec8550aae333dddcf5752db
nami-s <na.s1222.10feettt@gmail.com>
2021-04-03 10:19:49 +0900

    [fix]要素数が変化するのを想定してlengthを使用

--- js/practice2.js
+++ js/practice2.js
@@ -3,8 +3,8 @@
 {
   const btn = document.getElementById('btn');
   btn.addEventListener('click',()=>{
-    const results = ['大吉', '中吉', '凶'];
-    const n = Math.floor(Math.random() * 3);
+    const results = ['大吉', '中吉', '凶', '末吉'];
+    const n = Math.floor(Math.random() * results.length);
     btn.textContent = results[n];
 
     // switch(n){
