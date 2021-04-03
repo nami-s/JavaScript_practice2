62ca2f8b12029de5727076ffdae7a5e685626c58
nami-s <na.s1222.10feettt@gmail.com>
2021-04-03 10:17:45 +0900

    [fix]配列で表示

--- js/practice2.js
+++ js/practice2.js
@@ -2,21 +2,21 @@
 
 {
   const btn = document.getElementById('btn');
-
   btn.addEventListener('click',()=>{
+    const results = ['大吉', '中吉', '凶'];
     const n = Math.floor(Math.random() * 3);
-    // btn.textContent = n;
+    btn.textContent = results[n];
 
-    switch(n){
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
+    // switch(n){
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
+    // }
   });
 }
