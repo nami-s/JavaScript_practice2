0ec65a1bbc5637506c7e71da497cbc63a48e31a6
nami-s <na.s1222.10feettt@gmail.com>
2021-04-07 17:26:21 +0900

    [fix]関数で処理をまとめる

--- js/practice3.js
+++ js/practice3.js
@@ -1,13 +1,22 @@
 'use strict';
 
 {
-  const source = [];
-  for(let i = 0; i < 15; i++){
-    source[i] = i + 1;
+  function createColumn(col){
+    const source = [];
+    for(let i = 0; i < 15; i++){
+      source[i] = i + 1 + 15 * col;
+    }
+    const b = [];
+    for(let i = 0; i < 5; i++){
+      b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+    console.log(b);
   }
-  const b = [];
-  for(let i = 0; i < 5; i++){
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  }
-  console.log(b);
+
+  const columns = [];
+  columns[0] = createColumn(0)
+  columns[1] = createColumn(1)
+  columns[2] = createColumn(2)
+  columns[3] = createColumn(3)
+  columns[4] = createColumn(4)
 }
