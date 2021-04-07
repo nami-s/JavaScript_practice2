2ad869f8d63d0afd521b473cec8702648561ac2e
nami-s <na.s1222.10feettt@gmail.com>
2021-04-07 17:29:37 +0900

    [fix]定数名

--- js/practice3.js
+++ js/practice3.js
@@ -6,17 +6,17 @@
     for(let i = 0; i < 15; i++){
       source[i] = i + 1 + 15 * col;
     }
-    const b = [];
+    const column = [];
     for(let i = 0; i < 5; i++){
-      b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
     }
-    console.log(b);
+    return column;
   }
 
   const columns = [];
-  columns[0] = createColumn(0)
-  columns[1] = createColumn(1)
-  columns[2] = createColumn(2)
-  columns[3] = createColumn(3)
-  columns[4] = createColumn(4)
+  columns[0] = createColumn(0);
+  columns[1] = createColumn(1);
+  columns[2] = createColumn(2);
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(4);
 }
