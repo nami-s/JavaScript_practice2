a09f69ec80606474fa8eb4e49670363a30bcad89
nami-s <na.s1222.10feettt@gmail.com>
2021-04-24 14:20:33 +0900

    [fin]3択クイズ

--- quiz/practice5.js
+++ quiz/practice5.js
@@ -4,15 +4,19 @@
   const question = document.getElementById('question');
   const choices = document.getElementById('choices');
   const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
 
-  const quizSet = [
-    {q: 'What is this?', c: ['A', 'B', 'C']},
-    {q: 'What is that?', c: ['1', '2', '3']},
-    {q: 'What is A?', c: ['a', 'b', 'c']}
-  ];
+  const quizSet = shuffle([
+    {q: '幼い頃に天然痘により右目を失明し隻眼となり「独眼竜」の異名を持つ、仙台藩の初代藩主となった武将は？', c: ['伊達政宗', '石田三成', '前田慶次']},
+    {q: '25000人もの大軍を率いて京都に進出する途中、奇襲により桶狭間の戦いで織田信長に敗れた武将は？', c: ['今川義元', '真田幸村', '毛利元就']},
+    {q: '5度行われた川中島の戦いで武田信玄と戦った、越後の戦国武将は？', c: ['上杉謙信', '北条氏康', '三好長慶']},
+    {q: '信長への奉公ぶりが賞賛された逸話が多く残る、15歳から信長に仕え小姓となり、寵愛された武将は？', c: ['森蘭丸', '支倉常長', '加藤清正']}
+  ]);
 
   let countNum = 0;
   let isAnswered;
+  let score = 0;
 
   function shuffle(arr){
     for(let i = arr.length - 1; i > 0; i--){
@@ -29,6 +33,7 @@
     isAnswered = true;
     if(li.textContent === quizSet[countNum].c[0]){
       li.classList.add('correct');
+      score++;
     } else{
       li.classList.add('wrong');
     }
@@ -66,8 +71,13 @@
       return;
     }
     btn.classList.add('disabled');
-    countNum++;
-    setQuiz();
-  });
 
+    if(countNum === quizSet.length -1){
+      result.classList.remove('hidden');
+      scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`
+    } else{
+      countNum++;
+      setQuiz();
+    }
+  });
 }
