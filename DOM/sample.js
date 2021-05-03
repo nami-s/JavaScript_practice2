c324e7b4ca8f6c126d78289929e14040203c87fc
nami-s <na.s1222.10feettt@gmail.com>
2021-05-03 18:11:34 +0900

    [add]要素の操作記述使用例

--- DOM/sample.js
+++ DOM/sample.js
@@ -1,5 +1,11 @@
 'use strict';
 
 {
-  
+  function update(){
+    // document.querySelector('h1').textContent = 'changed!!!';    // querySelector()はDOM（document）から特定の要素を取得するメソッド　
+    // document.querySelector('#target').textContent = 'changed!!';    // id属性で取得することも可能
+    document.getElementById('target').textContent = 'changed!';    // getElementById()はidを指定して要素を探すメソッド
+  }
+
+  setTimeout(update, 1000);  // updateを1秒後に呼び出す
 }
