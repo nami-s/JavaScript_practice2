effe6534f4c4025ebc402f1f74a1987c653c12ac
nami-s <na.s1222.10feettt@gmail.com>
2021-05-03 18:35:38 +0900

    [add]複数要素の取得記述使用例

--- DOM/sample.js
+++ DOM/sample.js
@@ -5,6 +5,10 @@
     // document.querySelector('h1').textContent = 'changed!!!';    // querySelector()はDOM（document）から特定の要素を取得するメソッド　
     // document.querySelector('#target').textContent = 'changed!!';    // id属性で取得することも可能
     document.getElementById('target').textContent = 'changed!';    // getElementById()はidを指定して要素を探すメソッド
+    // document.querySelectorAll('p')[1].textContent = 'change!';    // querySelectorAllで引数の要素全てをDOMから取得し何番目の要素を操作したいかインデックス番号で指定
+    document.querySelectorAll('p').forEach((p, index) => {
+      p.textContent = `${index}番目のp要素`;
+    });
   }
 
   setTimeout(update, 1000);  // updateを1秒後に呼び出す
