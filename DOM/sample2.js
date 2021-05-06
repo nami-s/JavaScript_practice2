c3ca14cc6952d82e71e64586360acd6765b0ca0c
nami-s <na.s1222.10feettt@gmail.com>
2021-05-06 13:34:45 +0900

    [fix]

--- DOM/sample2.js
+++ DOM/sample2.js
@@ -4,11 +4,10 @@
   document.querySelector('button').addEventListener('click', () =>{    // クリックイベント
     const targetNode = document.getElementById('target');
 
-    targetNode.textContent = 'Change';
-    targetNode.title = 'This is title';    // 要素の属性を操作している（例外を除いてDOMは属性と同名のプロパティが用意されている）
+    // targetNode.textContent = 'Change';
+    // targetNode.title = 'This is title';    // 要素の属性を操作している（例外を除いてDOMは属性と同名のプロパティが用意されている）
     // targetNode.className = 'my-color';    // classNameはclass属性を丸々書き換えるもの
     // targetNode.className = 'my-color my-border';    // 元々付与されているclass属性も引き継ぎたい場合の書き方
-    
     // targetNode.classList.add('my-color');    // 元々付与されているclass属性を持ちながら新たなクラスを付与する書き方
 
     if(targetNode.classList.contains('my-color') === true){      // classList.contains()は引数のクラスが付いているかどうかboolで返してくれる
