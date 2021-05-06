e6afbb09c0155463644a50ba83f43b7e57b54015
nami-s <na.s1222.10feettt@gmail.com>
2021-05-06 13:38:46 +0900

    [add]classList使用記述例

--- DOM/sample2.js
+++ DOM/sample2.js
@@ -9,11 +9,13 @@
     // targetNode.className = 'my-color';    // classNameはclass属性を丸々書き換えるもの
     // targetNode.className = 'my-color my-border';    // 元々付与されているclass属性も引き継ぎたい場合の書き方
     // targetNode.classList.add('my-color');    // 元々付与されているclass属性を持ちながら新たなクラスを付与する書き方
+    // if(targetNode.classList.contains('my-color') === true){      // classList.contains()は引数のクラスが付いているかどうかboolで返してくれる
+    //   targetNode.classList.remove('my-color');      // classList.remove()で引数のクラスを外す
+    // } else {
+    //   targetNode.classList.add('my-color');
+    // }
 
-    if(targetNode.classList.contains('my-color') === true){      // classList.contains()は引数のクラスが付いているかどうかboolで返してくれる
-      targetNode.classList.remove('my-color');      // classList.remove()で引数のクラスを外す
-    } else {
-      targetNode.classList.add('my-color');
-    }
+    // 上のif文の省略形
+    targetNode.classList.toggle('my-color');
   });
 }
