392ee5e85367867ca57bf2da70b861e0403ecf9e
nami-s <na.s1222.10feettt@gmail.com>
2021-05-06 15:49:19 +0900

    [fix]

--- DOM/sample3.js
+++ DOM/sample3.js
@@ -2,10 +2,11 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const item2 = document.createElement('li');    // .createElement()　要素を作成するメソッド
-    item2.textContent = 'item 2';
-
+    const item3 = document.createElement('li');    // .createElement()　要素を作成するメソッド
+    item3.textContent = 'item 3';
     const ul = document.querySelector('ul');    // 親要素の呼び出し
-    ul.appendChild(item2);    // appendChild()　子要素の末尾に要素を追加するメソッド
+    ul.appendChild(item3);    // appendChild()　子要素の末尾に要素を追加するメソッド
+
+    
   });
 }
