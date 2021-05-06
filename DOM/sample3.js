0eb851117bc10bc9021d26e55247b55f12bb0511
nami-s <na.s1222.10feettt@gmail.com>
2021-05-06 14:32:33 +0900

    [add]要素の作成追加使用記述例

--- DOM/sample3.js
+++ DOM/sample3.js
@@ -2,6 +2,10 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
+    const item2 = document.createElement('li');    // .createElement()　要素を作成するメソッド
+    item2.textContent = 'item 2';
 
+    const ul = document.querySelector('ul');    // 親要素の呼び出し
+    ul.appendChild(item2);    // appendChild()　子要素の末尾に要素を追加するメソッド
   });
 }
