01c9f200a02308373296926672cf4d2931b89714
nami-s <na.s1222.10feettt@gmail.com>
2021-05-06 12:03:13 +0900

    [add]addEventListener使用記述例

--- DOM/sample2.js
+++ DOM/sample2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 {
-  document.querySelector('button').addEventListener('click', () =>{
-    document.getElementById('target').textContent = 'Change';
+  document.querySelector('button').addEventListener('click', () =>{    // クリックイベント
+    document.getElementById('target').textContent = 'Change';    // ボタンをクリックした時に行われる処理
   });
 }
