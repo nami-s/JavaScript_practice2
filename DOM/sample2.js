efb6476d59824ff3164f80618dde3b242cbbe6d3
nami-s <na.s1222.10feettt@gmail.com>
2021-05-06 13:14:54 +0900

    [fix]

--- DOM/sample2.js
+++ DOM/sample2.js
@@ -6,7 +6,7 @@
 
     targetNode.textContent = 'Change';
     targetNode.title = 'This is title';    // 要素の属性を操作している（例外を除いてDOMは属性と同名のプロパティが用意されている）
-    targetNode.className = 'my-color';    // classNameはclass属性を丸々書き換えるもの
-    targetNode.className = 'my-color my-border';    // 元々付与されているclass属性も引き継ぎたい場合の書き方
+    // targetNode.className = 'my-color';    // classNameはclass属性を丸々書き換えるもの
+    // targetNode.className = 'my-color my-border';    // 元々付与されているclass属性も引き継ぎたい場合の書き方
   });
 }
