21fc89934e7ef8bee3fc714c9ddef9fa283f1ea9
nami-s <na.s1222.10feettt@gmail.com>
2021-05-06 12:32:26 +0900

    [add]title属性をJSから操作する記述使用例

--- DOM/sample2.js
+++ DOM/sample2.js
@@ -5,6 +5,6 @@
     const targetNode = document.getElementById('target');
 
     targetNode.textContent = 'Change';
-    targetNode.title = 'This is title';
+    targetNode.title = 'This is title';    // 要素の属性を操作している（例外を除いてDOMは属性と同名のプロパティが用意されている）
   });
 }
