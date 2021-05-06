9b90e17bcd027fd3c15bca558f158a0f8ec19543
nami-s <na.s1222.10feettt@gmail.com>
2021-05-06 14:02:06 +0900

    [add]カスタムデータ属性使用記述例

--- DOM/sample3.js
+++ DOM/sample3.js
@@ -3,5 +3,7 @@
 {
   document.querySelector('button').addEventListener('click', () =>{
     const targetNode = document.getElementById('target');
+
+    targetNode.textContent = targetNode.dataset.read;    // カスタムデータ属性呼び出し方
   });
 }
