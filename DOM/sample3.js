56c1b9f8c801f43136fbf874e8c313db3e2ae529
nami-s <na.s1222.10feettt@gmail.com>
2021-05-11 18:45:55 +0900

    [add]要素の削除記述使用例

--- DOM/sample3.js
+++ DOM/sample3.js
@@ -11,5 +11,8 @@
     const copy = item0.cloneNode(true);    // .cloneNode()　要素の複製をするメソッドで引数をtrueとすることで要素の中身も複製できる
     const item2 = document.querySelectorAll('li')[2];
     ul.insertBefore(copy, item2)    // insertBefore()　要素を挿入するメソッド（第1引数に挿入する要素、第2引数に挿入する場所の次の要素を指定）
+
+    const item1 = document.querySelectorAll('li')[1];
+    item1.remove();    // 要素を削除するメソッド
   });
 }
