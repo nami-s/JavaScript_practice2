c010e6a2ebd2561669addb5769bf4f001aa65160
nami-s <na.s1222.10feettt@gmail.com>
2021-05-13 11:49:22 +0900

    [fix]

--- DOM/sample4.js
+++ DOM/sample4.js
@@ -1,15 +1,15 @@
 'use strict'
 
-{
-  document.querySelector('button').addEventListener('click', () => {
-    const li = document.createElement('li');    // li要素の作成
-    const text = document.querySelector('input');    // input要素の取得
-    const color = document.querySelector('select');    // select要素の取得
+// {
+//   document.querySelector('button').addEventListener('click', () => {
+//     const li = document.createElement('li');    // li要素の作成
+//     const text = document.querySelector('input');    // input要素の取得
+//     const color = document.querySelector('select');    // select要素の取得
 
-    li.textContent = `${text.value} : ${color.value} - ${color.selectedIndex}`;    // 入力された値、選択された値、選択された値のインデックス番号を代入
-    document.querySelector('ul').appendChild(li);    // ulの子要素にliを追加
+//     li.textContent = `${text.value} : ${color.value} - ${color.selectedIndex}`;    // 入力された値、選択された値、選択された値のインデックス番号を代入
+//     document.querySelector('ul').appendChild(li);    // ulの子要素にliを追加
 
-    text.value = '';    // 入力欄をクリアにする
-    text.focus();    // 入力欄（input要素）にフォーカスを当てる
-  });
-}
+//     text.value = '';    // 入力欄をクリアにする
+//     text.focus();    // 入力欄（input要素）にフォーカスを当てる
+//   });
+// }
