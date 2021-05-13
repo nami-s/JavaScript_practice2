554b55a8daea19cfd05b44a34294af9b79591cee
nami-s <na.s1222.10feettt@gmail.com>
2021-05-13 11:43:00 +0900

    [add]セレクトボックス使用記述例

--- DOM/sample4.js
+++ DOM/sample4.js
@@ -4,6 +4,9 @@
   document.querySelector('button').addEventListener('click', () => {
     const li = document.createElement('li');    // li要素の作成
     const text = document.querySelector('input');    // input要素の取得
+    const color = document.querySelector('select');    // select要素の取得
+
+    li.textContent = `${text.value} : ${color.value} - ${color.selectedIndex}`;    // 入力された値、選択された値、選択された値のインデックス番号を代入
     document.querySelector('ul').appendChild(li);    // ulの子要素にliを追加
 
     text.value = '';    // 入力欄をクリアにする
