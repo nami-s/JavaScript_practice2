9bac8c741ba9924f3569afb86252ac78a30f30f8
nami-s <na.s1222.10feettt@gmail.com>
2021-05-13 11:02:48 +0900

    [add]input要素の操作の仕方記述例

--- DOM/sample4.js
+++ DOM/sample4.js
@@ -1,1 +1,13 @@
 'use strict'
+
+{
+  document.querySelector('button').addEventListener('click', () => {
+    const li = document.createElement('li');    // li要素の作成
+    const text = document.querySelector('input');    // input要素の取得
+    li.textContent = text.value;    // input要素で入力された値を取得して、liのテキストに代入
+    document.querySelector('ul').appendChild(li);    // ulの子要素にliを追加
+
+    text.value = '';    // 入力欄をクリアにする
+    text.focus();    // 入力欄（input要素）にフォーカスを当てる
+  });
+}
