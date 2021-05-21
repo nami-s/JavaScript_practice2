48fed7a65a23b439484549c8d572678c5dbfd289
nami-s <na.s1222.10feettt@gmail.com>
2021-05-21 13:12:27 +0900

    [add]チェックボックス使用記述例

--- DOM/sample5.js
+++ DOM/sample5.js
@@ -1,5 +1,17 @@
 'use strict';
 
 {
-  
+  document.querySelector('button').addEventListener('click', ()=> {
+     const colors = document.querySelectorAll('input');
+     const selectedColors = [];
+     colors.forEach(color => {
+       if(color.checked === true){
+         selectedColors.push(color.value);
+       }
+     });
+
+     const li = document.createElement('li');
+     li.textContent = selectedColors;
+     document.querySelector('ul').appendChild(li);
+  });
 }
