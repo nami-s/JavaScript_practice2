adb146a08e27df86fded1190426f44af2fa0f42b
nami-s <na.s1222.10feettt@gmail.com>
2021-05-22 13:19:00 +0900

    [add]キーボードのキーを押した時のイベント記述使用例

--- DOM/sample6.js
+++ DOM/sample6.js
@@ -9,4 +9,8 @@
     // console.log('マウスを動かしました');
     console.log(e.clientX,e.clientY);    // ブラウザの表示領域のXY座標を表示
   });
+
+  document.addEventListener('keydown', e => {    // キーボードのキーを押した時のイベント
+    console.log(e.key);    // 押したキーを取得
+  });
 }
