3bbaba4d01b3a325ef7672c7590e03a765ac027e
nami-s <na.s1222.10feettt@gmail.com>
2021-05-22 13:06:19 +0900

    [add]イベントオブジェクト使用記述例

--- DOM/sample6.js
+++ DOM/sample6.js
@@ -5,7 +5,8 @@
     console.log('ダブルクリック');
   });
 
-  document.addEventListener('mousemove', () =>{    // マウスを動かした時のイベント
-    console.log('マウスを動かしました');
+  document.addEventListener('mousemove', e =>{    // マウスを動かした時のイベント
+    // console.log('マウスを動かしました');
+    console.log(e.clientX,e.clientY);    // ブラウザの表示領域のXY座標を表示
   });
 }
