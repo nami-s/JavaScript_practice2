917e61f53bebee8fa0ae2f42b74f0b45d0e0548e
nami-s <na.s1222.10feettt@gmail.com>
2021-05-22 12:40:18 +0900

    [add]マウスを動かした時のイベント記述使用例

--- DOM/sample6.js
+++ DOM/sample6.js
@@ -4,4 +4,8 @@
   document.querySelector('button').addEventListener('dblclick', ()=>{    // ダブルクリックイベント
     console.log('ダブルクリック');
   });
+
+  document.addEventListener('mousemove', () =>{    // マウスを動かした時のイベント
+    console.log('マウスを動かしました');
+  });
 }
