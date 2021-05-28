0aacdcacfa5dc20512e80a64eda26694f71e53c0
nami-s <na.s1222.10feettt@gmail.com>
2021-05-28 17:51:25 +0900

    [add]inputイベント記述使用例

--- DOM/sample7.js
+++ DOM/sample7.js
@@ -3,11 +3,15 @@
 {
   const text = document.querySelector('textarea');
 
-  text.addEventListener('focus', () =>{
+  text.addEventListener('focus', () =>{    // フォーカスが当たった時のイベント
     console.log('focus');
   });
 
-  text.addEventListener('blur', () =>{
+  text.addEventListener('blur', () =>{    // フォーカスが外れた時のイベント
     console.log('blur');
+  });
+  
+  text.addEventListener('input', () =>{    // 文字が入力されるたびに発生するイベント
+    console.log('input');
   });
 }
