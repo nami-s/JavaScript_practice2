00730c2b6d9925baf0d1d4e9b8ae2e06aaaeb404
nami-s <na.s1222.10feettt@gmail.com>
2021-05-28 17:58:34 +0900

    [add]inputイベント使用記述例

--- DOM/sample7.js
+++ DOM/sample7.js
@@ -13,6 +13,7 @@
   
   text.addEventListener('input', () =>{    // 文字が入力されるたびに発生するイベント
     console.log('input');
+    console.log(`${text.value.length}文字`);    // テキストエリアに入力された文字を取得して文字数を表示
   });
 
   text.addEventListener('change', () =>{    // 文字入力後フォーカスを外した時のイベント
