a23261d0b6dde097a6887dd7d28fde8e77328092
nami-s <na.s1222.10feettt@gmail.com>
2021-06-04 15:36:40 +0900

    [add]preventDefault使用記述例

--- DOM/sample8.js
+++ DOM/sample8.js
@@ -1,7 +1,8 @@
 'use strict';
 
 {
-  document.querySelector('form').addEventListener('submit', () => {    // submitイベント（formタグ内のボタンをクリックしたときに発生する）
+  document.querySelector('form').addEventListener('submit', e => {    // submitイベント（formタグ内のボタンをクリックしたときに発生する）
+    e.preventDefault();    // 既定の動作をキャンセルするメソッド（この場合は画面遷移をキャンセルしている）
     console.log('submit');
   });
 }
